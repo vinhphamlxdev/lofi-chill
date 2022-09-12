0dd24d3a0e35d81de6c4298cd8e990dab6c751ed
vinh <vinhphamdev0406@gmail.com>
2022-09-12 23:09:52 +0700

    update

--- src/songData/index.js
+++ src/songData/index.js
@@ -10,75 +10,117 @@ import rap2 from "~/assets/songs/rap2.mp3";
 import rap3 from "~/assets/songs/rap3.mp3";
 import rap4 from "~/assets/songs/rap4.mp3";
 import rap5 from "~/assets/songs/rap5.mp3";
+import rap6 from "~/assets/songs/rap6.mp3";
+import rap7 from "~/assets/songs/rap7.mp3";
 import sleep1 from "~/assets/songs/sleep.mp3";
 export const chill = [
   {
     id: 1,
     name: "Ai Đợi Mình Được Mãi (Lofi Ver.)",
-    src: `${chill1}`,
+    src: chill1,
   },
   {
     id: 2,
     name: "Anh Vẫn Ở Đây (Lofi Ver.)",
-    src: `${chill2}`,
+    src: chill2,
   },
   {
     id: 3,
     name: "Chẳng Thể Tìm Được Em (Lofi Ver)",
-    src: `${chill3}`,
+    src: chill3,
   },
   {
     id: 4,
     name: "Pastlives",
-    src: `${chill4}`,
+    src: chill4,
   },
   {
     id: 5,
     name: "Đào Nương (Lofi Ver)",
-    src: `${chill5}`,
+    src: chill5,
   },
   {
     id: 6,
     name: "Cảm Ơn Vì Tất Cả (Lofi Ver)",
-    src: `${chill6}`,
+    src: chill6,
   },
   {
-    id: 6,
+    id: 7,
     name: "Kiếp Má Hồng (Lofi Ver.)",
-    src: `${chill7}`,
+    src: chill7,
   },
 ];
 export const rap = [
   {
     id: 1,
     name: "Không Phải Tại Nó - Mai Ngô",
-    src: `${rap1}`,
+    src: rap1,
   },
   {
     id: 2,
     name: "G.Ducky - Eyes",
-    src: `${rap2}`,
+    src: rap2,
   },
   {
     id: 3,
     name: "Đen - hai triệu năm ft. Biên",
-    src: `${rap3}`,
+    src: rap3,
   },
   {
     id: 4,
     name: "BINZ - OK ",
-    src: `${rap4}`,
+    src: rap4,
   },
   {
     id: 5,
     name: "Đen ft. MIN - Bài Này Chill Phết",
-    src: `${rap5}`,
+    src: rap5,
+  },
+  {
+    id: 6,
+    name: "HIEUTHUHAI - CHƠI (RMX) Ft.MANBO",
+    src: rap6,
+  },
+  {
+    id: 7,
+    name: "CUA - HIEUTHUHAI",
+    src: rap7,
   },
 ];
 export const sleep = [
   {
     id: 1,
     name: "sleep sleep sleep",
-    src: `${sleep1}`,
+    src: sleep1,
+  },
+  {
+    id: 2,
+    name: "sleep sleep sleep",
+    src: sleep1,
+  },
+  {
+    id: 3,
+    name: "sleep sleep sleep",
+    src: sleep1,
+  },
+  {
+    id: 4,
+    name: "sleep sleep sleep",
+    src: sleep1,
+  },
+  {
+    id: 5,
+    name: "sleep sleep sleep",
+    src: sleep1,
+  },
+  {
+    id: 6,
+    name: "sleep sleep sleep",
+    src: sleep1,
+  },
+  {
+    id: 7,
+    name: "sleep sleep sleep",
+    src: sleep1,
   },
 ];
